d7e6f0a76cb8edbe9f5ef8f0fbc51c54b23c4475
Nicolas <nicolas.mine11@gmail.com>
2021-12-06 15:28:03 +0100

    rename test

--- polls/test/infrastructure/RetrieveMembersUseCaseTest.py
+++ polls/test/infrastructure/RetrieveMembersUseCaseTest.py
@@ -7,12 +7,15 @@ from polls.domain.Member import Member
 from polls.domain.MembersRepository import MembersRepository
 
 
-class IndexTests(SimpleTestCase):
+class RetrieveMembersUseCaseTest(SimpleTestCase):
 
-    def test_index_no_results(self):
+    def test_retrieve_members_useCase(self):
+        # Given
         repository = MembersRepository()
         member = Member('test', 'test')
         repository.retrieve_member = MagicMock(return_value=member)
-        useCase = RetrieveMembersUseCase(repository)
-        result = useCase.execute()
+        # WHen
+        usecase = RetrieveMembersUseCase(repository)
+        result = usecase.execute()
+        # Then
         self.assertEqual(result.firstname, "test")
