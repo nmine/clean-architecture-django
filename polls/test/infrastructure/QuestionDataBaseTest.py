0a0495815133261f1ee45747f5cbc772707add66
Nicolas <nicolas.mine11@gmail.com>
2021-12-07 10:01:29 +0100

    add views tests

--- polls/test/infrastructure/QuestionDataBaseTest.py
+++ polls/test/infrastructure/QuestionDataBaseTest.py
@@ -1,6 +1,7 @@
-from django.core.mail.backends import console
-from django.test import TestCase
 import datetime
+
+from django.test import TestCase
+
 from polls.infrastructure.models.models import Question
 
 
@@ -10,3 +11,15 @@ class AnimalTestCase(TestCase):
 
     def test_animals_can_speak(self):
         question = Question.objects.get(question_text="lion")
+        field_label = question._meta.get_field('question_text').verbose_name
+        self.assertEqual(field_label, 'question text')
+
+    def test_first_name_max_length(self):
+        question = Question.objects.get(question_text="lion")
+        max_length = question._meta.get_field('question_text').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Question.objects.get(question_text="lion")
+        expected_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEqual(str(author), expected_object_name)
--- polls/test/infrastructure/ViewsTest.py
+++ polls/test/infrastructure/ViewsTest.py
@@ -1,7 +1,11 @@
+import json
+
 from django.test import TestCase
 
 
 class TestCalls(TestCase):
-    def test_call_view_deny_anonymous(self):
-        response = self.client.get('/polls', follow=True)
-        self.assertRedirects(response, '/login/')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "Hello, world. You're at the polls index.")
