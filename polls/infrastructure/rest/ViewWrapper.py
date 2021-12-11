224b834e345263f7a85b67a75e988ea32e7d3e05
Nicolas <nicolas.mine11@gmail.com>
2021-12-11 06:56:12 +0100

    change url .py to add factory

--- polls/infrastructure/rest/ViewWrapper.py
+++ polls/infrastructure/rest/ViewWrapper.py
@@ -5,6 +5,6 @@ from django.views import View
 class ViewWrapper(View):
     usecaseFactory = None
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, question_id: int, question_id2: int):
         member = self.usecaseFactory.get().execute()
         return HttpResponse("You're looking at question.")
